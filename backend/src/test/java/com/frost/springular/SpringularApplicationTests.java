0a914930abec36aa3be68600a128083695e9daec
chopsticks-user <frostyfrost273@gmail.com>
2024-06-14 14:55:11 -0400

    Initialize springular

---
+++ backend/src/test/java/com/frost/springular/SpringularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frost.springular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
