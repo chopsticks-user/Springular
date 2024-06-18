af479ff533a968a25ef290927c594d368ce0f078
chopsticks-user <frostyfrost273@gmail.com>
2024-06-17 20:29:16 -0400

    Move ThemeService up

--- frontend/src/app/components/welcome/welcome.component.ts
+++ frontend/src/app/components/welcome/welcome.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { ThemeService } from '../../services/theme/theme.service';
+import { ThemeService } from '../../services/theme.service';
 
 @Component({
   selector: 'app-welcome',
