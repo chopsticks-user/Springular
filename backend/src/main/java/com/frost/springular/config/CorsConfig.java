8a0780ae9c5e47bfb9dd5d8b1550cff8c1daecec
chopsticks-user <frostyfrost273@gmail.com>
2024-06-20 21:13:07 -0400

    Add spring jpa and postgresql

--- backend/src/main/java/com/frost/springular/config/CorsConfig.java
+++ backend/src/main/java/com/frost/springular/config/CorsConfig.java
@@ -13,9 +13,9 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
+                registry.addMapping("/api/**")
                         .allowedOrigins("http://localhost:4200")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
