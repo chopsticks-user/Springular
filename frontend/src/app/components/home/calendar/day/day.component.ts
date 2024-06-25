86f6ce39499fb90ed7d143a65b6faca0524baa77
chopsticks-user <frostyfrost273@gmail.com>
2024-06-25 14:34:16 -0400

    Add calendar

---
+++ frontend/src/app/components/home/calendar/day/day.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-calendar-day',
+  standalone: true,
+  imports: [],
+  templateUrl: './day.component.html',
+  styleUrl: './day.component.css',
+})
+export class CalendarDayComponent {}
