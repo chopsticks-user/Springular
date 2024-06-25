84bf02b807ac32e846b8ed4585e35b19bbc3b089
chopsticks-user <frostyfrost273@gmail.com>
2024-06-25 12:47:28 -0400

    Homepage layout

--- frontend/src/app/components/home/calendar/calendar.component.ts
+++ frontend/src/app/components/home/calendar/calendar.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-calendar',
+  selector: 'app-home-calendar',
   standalone: true,
   imports: [],
   templateUrl: './calendar.component.html',
-  styleUrl: './calendar.component.css'
+  styleUrl: './calendar.component.css',
 })
-export class CalendarComponent {
-
-}
+export class CalendarComponent {}
--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '@services/auth.service';
+import { CalendarComponent } from './calendar/calendar.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent, HeaderComponent, CalendarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
