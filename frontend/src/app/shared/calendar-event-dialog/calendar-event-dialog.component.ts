0e394515a83c955493708880ddba910904048e78
chopsticks-user <frostyfrost273@gmail.com>
2024-07-03 11:43:55 -0400

    Add eventFormGroup

--- frontend/src/app/shared/calendar-event-dialog/calendar-event-dialog.component.ts
+++ frontend/src/app/shared/calendar-event-dialog/calendar-event-dialog.component.ts
@@ -1,11 +1,4 @@
-import {
-  Component,
-  ElementRef,
-  EventEmitter,
-  Input,
-  Output,
-  ViewChild,
-} from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import {
   FormControl,
   FormGroup,
@@ -13,11 +6,12 @@ import {
   Validators,
 } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { CalendarEvent } from '@shared/types';
-import { Observable } from 'rxjs';
-import { MatSelect, MatSelectModule } from '@angular/material/select';
+import { Observable, map, startWith } from 'rxjs';
+import { MatSelectModule } from '@angular/material/select';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-shared-calendar-event-dialog',
@@ -28,24 +22,64 @@ import { MatSelect, MatSelectModule } from '@angular/material/select';
     MatInputModule,
     MatIconModule,
     MatSelectModule,
+    AsyncPipe,
   ],
   templateUrl: './calendar-event-dialog.component.html',
   styleUrl: './calendar-event-dialog.component.css',
 })
 export class CalendarEventDialogComponent {
-  @Input() public calendarEvent!: Observable<CalendarEvent>;
+  @Input() public calendarEvent$!: Observable<CalendarEvent>;
   @Output() public $cancelButtonClicked = new EventEmitter();
   @Output() public $submitButtonClicked = new EventEmitter();
 
   public eventFormGroup: FormGroup = new FormGroup({
-    title: new FormControl('', [Validators.required]),
-    description: new FormControl('', [Validators.required]),
-    start: new FormControl('', [Validators.required]),
-    end: new FormControl('', [Validators.required]),
-    repeat: new FormControl('None', []),
+    title: new FormControl<string>('', [Validators.required]),
+    description: new FormControl<string>('', [Validators.required]),
+    start: new FormControl<string>('', [Validators.required]),
+    end: new FormControl<string>('', [Validators.required]),
+    color: new FormControl<string>('green', [Validators.required]),
+    repeat: new FormControl<string>('None', []),
   });
 
+  public get repeatEveryEnabled$(): Observable<boolean> {
+    return this.eventFormGroup.get('repeat')?.valueChanges.pipe(
+      startWith(this.eventFormGroup.get('repeat')?.value),
+      map((value: string | null) => {
+        if (!value || value.toLowerCase() !== 'custom') {
+          this.eventFormGroup.removeControl('repeatEvery');
+          return false;
+        }
+
+        this.eventFormGroup.addControl(
+          'repeatEvery',
+          new FormControl<string>('', [Validators.required])
+        );
+        return true;
+      })
+    ) as Observable<boolean>;
+  }
+
   public submitEvent(): void {
-    console.log('submitted');
+    if (this.eventFormGroup.valid) {
+      const calendarEvent: CalendarEvent = {
+        title: this.eventFormGroup.get('title')?.value,
+        description: this.eventFormGroup.get('description')?.value,
+        color: this.eventFormGroup.get('color')?.value,
+        start: this.eventFormGroup.get('start')?.value,
+        durationMinutes: 60,
+        repeat: this.eventFormGroup.get('repeat')?.value,
+      };
+
+      if (this.eventFormGroup.contains('repeatEvery')) {
+        calendarEvent.repeatEvery = {
+          value: 60,
+          unit: this.eventFormGroup.get('repeatEvery')?.value,
+        };
+      }
+
+      return console.log(calendarEvent);
+    }
+
+    console.log('Invalid form');
   }
 }
