31db9a12e5b9d76507624db5fba93ec2205c7cb5
chopsticks-user <frostyfrost273@gmail.com>
2024-07-14 08:36:17 -0400

    Basic layout

--- frontend/src/app/components/home/sidebar/sidebar.component.ts
+++ frontend/src/app/components/home/sidebar/sidebar.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { UserSectionComponent } from './user-section/user-section.component';
+import { NavigationSectionComponent } from './navigation-section/navigation-section.component';
+
+interface SidebarItem {
+  name: string;
+  icon: string;
+}
+
+interface SidebarSection {
+  id: number;
+  name?: string;
+  items: SidebarItem[];
+}
 
 @Component({
   selector: 'app-home-sidebar',
   standalone: true,
-  imports: [],
+  imports: [UserSectionComponent, NavigationSectionComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css',
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  public readonly sections: SidebarSection[] = [];
+}
