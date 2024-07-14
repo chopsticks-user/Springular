4ab94dbc1d57b3b37b9b4d70b7ebba5c7746d7af
chopsticks-user <frostyfrost273@gmail.com>
2024-07-14 14:01:16 -0400

    Finance basic layout

---
+++ frontend/src/app/components/home/finance/balance-summary/balance-summary.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-finance-balance-summary',
+  standalone: true,
+  imports: [],
+  templateUrl: './balance-summary.component.html',
+  styleUrl: './balance-summary.component.css',
+})
+export class BalanceSummaryComponent {}
