9fa7b19dedf34a2bf3318186335de8f4bd47ac29
chopsticks-user <frostyfrost273@gmail.com>
2024-07-19 11:52:13 -0400

    GET methods for finance/groups and finance/transactions

--- server/src/main/java/com/frost/springular/controller/FinanceController.java
+++ server/src/main/java/com/frost/springular/controller/FinanceController.java
@@ -1,10 +1,53 @@
 package com.frost.springular.controller;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.frost.springular.object.model.TransactionGroupModel;
+import com.frost.springular.object.response.TransactionGroupResponse;
+import com.frost.springular.object.response.TransactionResponse;
+import com.frost.springular.service.FinanceService;
+import com.frost.springular.service.UserService;
 
 @RestController
 @RequestMapping("/api/finance")
 public class FinanceController {
+  private final UserService userService;
+  private final FinanceService financeService;
 
+  public FinanceController(UserService userService, FinanceService financeService) {
+    this.userService = userService;
+    this.financeService = financeService;
+  }
+
+  @GetMapping("/groups")
+  public ResponseEntity<List<TransactionGroupResponse>> getAllTransactionGroups() {
+    return ResponseEntity.ok(
+        this.financeService.getAllTransactionGroups(
+            this.userService.getCurrentUser())
+            .stream()
+            .map((groupModel) -> new TransactionGroupResponse(groupModel))
+            .toList());
+  }
+
+  @GetMapping("/transactions")
+  public ResponseEntity<List<TransactionResponse>> getTransactionsBetween(
+      @RequestParam String interval, @RequestParam Instant start) {
+    return ResponseEntity.ok(
+        this.financeService.filterTransactionsByInterval(
+            interval, start.truncatedTo(ChronoUnit.MONTHS),
+            this.userService.getCurrentUser())
+            .stream()
+            .map((transactionModel) -> new TransactionResponse(transactionModel))
+            .toList());
+  }
 }
--- server/src/main/java/com/frost/springular/service/FinanceService.java
+++ server/src/main/java/com/frost/springular/service/FinanceService.java
@@ -1,10 +1,13 @@
 package com.frost.springular.service;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.frost.springular.object.model.TransactionGroupModel;
+import com.frost.springular.object.model.TransactionModel;
 import com.frost.springular.object.model.UserModel;
 import com.frost.springular.object.repository.TransactionGroupRepository;
 import com.frost.springular.object.repository.TransactionRepository;
@@ -25,6 +28,27 @@ public class FinanceService {
     return transactionGroupRepository.findByUser(userModel);
   }
 
-  // public List<TransactionGroupModel> saveTransactionGroup() {
-  // }
+  public List<TransactionModel> filterTransactionsByInterval(
+      String interval, Instant startOfInterval, UserModel userModel) {
+    return switch (interval) {
+      case "month" -> this.transactionRepository.filterBetween(
+          userModel, startOfInterval,
+          startOfInterval.plus(1, ChronoUnit.MONTHS));
+      case "year" -> this.transactionRepository.filterBetween(
+          userModel, startOfInterval,
+          startOfInterval.plus(1, ChronoUnit.YEARS));
+      default -> this.transactionRepository.filterBetween(
+          userModel, startOfInterval,
+          startOfInterval.plus(1, ChronoUnit.MONTHS));
+    };
+  }
+
+  public TransactionModel save(TransactionModel transactionModel) {
+    return this.transactionRepository.save(transactionModel);
+  }
+
+  public TransactionGroupModel save(TransactionGroupModel transactionGroupModel) {
+    // todo: update path
+    return this.transactionGroupRepository.save(transactionGroupModel);
+  }
 }
