5c0f67e1c28117c67adcef421f95df69d66f8c1c
chopsticks-user <frostyfrost273@gmail.com>
2024-07-23 12:07:32 -0400

    Add node to tsconfig

--- client/src/app/environments/environment.dev.ts
+++ client/src/app/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 const appEnv = {
-  serverAddr: process.env['ANGULAR_PROD_SERVER_ADDRESS'],
-  apiRootAddr: process.env['ANGULAR_PROD_API_ROOT_ADDRESS'],
+  serverAddr: process.env['ANGULAR_PROD_SERVER_ADDRESS']!,
+  apiRootAddr: process.env['ANGULAR_PROD_API_ROOT_ADDRESS']!,
 };
 
 export default appEnv;
