845272db02e76014747ffae9786dfd36e06b6818
chopsticks-user <frostyfrost273@gmail.com>
2024-07-25 11:01:40 -0400

    Recent activities layout

--- client/src/app/components/home/finance/recent-activities/daily-transactions/daily-transactions.component.ts
+++ client/src/app/components/home/finance/recent-activities/daily-transactions/daily-transactions.component.ts
@@ -1,9 +1,12 @@
 import {Component} from '@angular/core';
+import {TransactionComponent} from "@components/home/finance/recent-activities/transaction/transaction.component";
 
 @Component({
   selector: 'app-home-finance-daily-transactions',
   standalone: true,
-  imports: [],
+  imports: [
+    TransactionComponent
+  ],
   templateUrl: './daily-transactions.component.html',
   styleUrl: './daily-transactions.component.css'
 })
--- client/src/app/components/home/finance/recent-activities/transaction/transaction.component.ts
+++ client/src/app/components/home/finance/recent-activities/transaction/transaction.component.ts
@@ -1,19 +1,22 @@
-import {Component, input} from '@angular/core';
-import {Transaction} from '@shared/types';
-import {DateTime} from "luxon";
+import {Component} from '@angular/core';
+import {MatIcon} from "@angular/material/icon";
+import {MatSuffix} from "@angular/material/form-field";
 
 @Component({
-  selector: 'app-home-finance-recent-activities-transaction',
+  selector: 'app-home-finance-transaction',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIcon,
+    MatSuffix
+  ],
   templateUrl: './transaction.component.html',
   styleUrl: './transaction.component.css',
 })
 export class TransactionComponent {
-  public transaction = input.required<Transaction>();
-
-  public get time(): string {
-    return DateTime.fromJSDate(this.transaction().time)
-      .toLocaleString(DateTime.TIME_SIMPLE);
-  }
+  // public transaction = input.required<Transaction>();
+  //
+  // public get time(): string {
+  //   return DateTime.fromJSDate(this.transaction().time)
+  //     .toLocaleString(DateTime.TIME_SIMPLE);
+  // }
 }
