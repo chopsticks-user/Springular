bc80115dc134683a094a65d47294b74bd76c7b23
chopsticks-user <frostyfrost273@gmail.com>
2024-07-25 09:18:00 -0400

    Remove some tests

--- client/src/app/components/home/finance/recent-activities/daily-transactions/daily-transactions.component.spec.ts
+++ client/src/app/components/home/finance/recent-activities/daily-transactions/daily-transactions.component.spec.ts
@@ -1,23 +1,19 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
-
-import {DailyTransactionsComponent} from './daily-transactions.component';
-
-describe('DailyTransactionsComponent', () => {
-  let component: DailyTransactionsComponent;
-  let fixture: ComponentFixture<DailyTransactionsComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [DailyTransactionsComponent]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(DailyTransactionsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+// describe('DailyTransactionsComponent', () => {
+//   let component: DailyTransactionsComponent;
+//   let fixture: ComponentFixture<DailyTransactionsComponent>;
+//
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [DailyTransactionsComponent]
+//     })
+//     .compileComponents();
+//
+//     fixture = TestBed.createComponent(DailyTransactionsComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+//
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
