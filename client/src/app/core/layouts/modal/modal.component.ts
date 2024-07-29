e958c74566ec50dc00e50f73d775563ff937c946
chopsticks-user <frostyfrost273@gmail.com>
2024-07-29 06:33:19 -0400

    Fix dialog keeps showing up

--- client/src/app/core/layouts/modal/modal.component.ts
+++ client/src/app/core/layouts/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import {Component, effect, ElementRef, input, output, ViewChild} from '@angular/core';
+import {Component, effect, ElementRef, input, output, viewChild} from '@angular/core';
 import {MatIcon} from "@angular/material/icon";
 
 @Component({
@@ -11,23 +11,26 @@ import {MatIcon} from "@angular/material/icon";
   styleUrl: './modal.component.css'
 })
 export class ModalComponent {
-  @ViewChild('modal')
-  private _modalRef!: ElementRef<HTMLDialogElement>;
+  private _modalRef = viewChild.required<ElementRef>('modal');
 
   public modalShouldOpen = input.required<boolean>();
   public modalShouldClose = output<void>();
 
   constructor() {
     effect(() => {
-      if (!this._modalRef) {
+      if (!this._modalRef()) {
         return;
       }
 
       if (this.modalShouldOpen()) {
-        this._modalRef.nativeElement.showModal();
+        this._modalRef().nativeElement.showModal();
       } else {
-        this._modalRef.nativeElement.close();
+        this._modalRef().nativeElement.close();
       }
     });
+  }
+
+  ngAfterViewInit() {
+
   }
 }
